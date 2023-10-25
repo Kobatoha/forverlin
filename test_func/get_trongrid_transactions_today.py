1dd2a4966950c111760173dcb4aacd56a0ffacde
Alexandra <kobatoha@gmail.com>
2023-10-25 16:34:55 +0300

    update trongrid parser transactions day\yesterday

--- test_func/get_trongrid_transactions_today.py
+++ test_func/get_trongrid_transactions_today.py
@@ -23,6 +23,7 @@ params = {
     'only_confirmed': True,
     'min_timestamp': min_timestamp,
     'max_timestamp': max_timestamp,
+    'order_by': 'block_timestamp,desc',
     'limit': 200
 }
 
@@ -31,11 +32,27 @@ headers = {"Authorization": f"Bearer {api_key}"}
 
 response = requests.get(url, params=params, headers=headers)
 num = 0
+reports = []
 
 if response.status_code == 200:
     transactions = response.json()["data"]
-    for transaction in transactions:
+    for tr in transactions:
         num += 1
-        print(num, transaction)
+        symbol = tr.get('token_info', {}).get('symbol')
+        fr = tr.get('from')
+        to = tr.get('to')
+        v = tr.get('value', '')
+        dec = -1 * int(tr.get('token_info', {}).get('decimals', '6'))
+        f = float(v[:dec] + '.' + v[dec:])
+        time_ = dt.datetime.fromtimestamp(float(tr.get('block_timestamp', '')) / 1000)
+        if fr != wallet_address:
+            reports.append(f"{time_.strftime('%H:%M')}: +{f:,.02f} from {fr[:3]}_{fr[-3:]}")
+        else:
+            reports.append(f"{time_.strftime('%H:%M')}: -{f:,.02f} to {to[:3]}_{to[-3:]}")
+
 else:
     print(f"Error: {response.status_code} - {response.text}")
+
+for i in reports:
+    print(i)
+
--- test_func/get_trongrid_transactions_yesterday.py
+++ test_func/get_trongrid_transactions_yesterday.py
@@ -1,39 +1,62 @@
 import requests
 import datetime as dt
+from config import trongrid_api_key
+import asyncio
 
 
-# TRON GRID
-api_key = '88b905ca-b432-4f4e-bd6a-09ed49629c4a'
-wallet_address = 'TCoWH8WJDUvXqtvv7xLCF1K6NP4ZoyW8nJ'
+async def get_trongrid_transactions_yesterday(wallet_address, wallet_name):
+    api_key = trongrid_api_key
+    wallet_address = wallet_address
+    wallet_name = wallet_name
 
-today = dt.datetime.now().date()
-yesterday = today - dt.timedelta(days=1)
+    today = dt.datetime.now().date()
+    yesterday = today - dt.timedelta(days=1)
 
-start_of_yesterday = dt.datetime.combine(yesterday, dt.time.min)
-end_of_yesterday = dt.datetime.combine(yesterday, dt.time.max)
+    start_of_yesterday = dt.datetime.combine(yesterday, dt.time.min)
+    end_of_yesterday = dt.datetime.combine(yesterday, dt.time.max)
 
-min_timestamp = int(start_of_yesterday.timestamp() * 1000)
-max_timestamp = int(end_of_yesterday.timestamp() * 1000)
+    min_timestamp = int(start_of_yesterday.timestamp() * 1000)
+    max_timestamp = int(end_of_yesterday.timestamp() * 1000)
 
-url = f"https://api.trongrid.io/v1/accounts/{wallet_address}/transactions/trc20"
+    url = f"https://api.trongrid.io/v1/accounts/{wallet_address}/transactions/trc20"
 
-params = {
-    'only_confirmed': True,
-    'min_timestamp': min_timestamp,
-    'max_timestamp': max_timestamp,
-    'limit': 200
-}
+    params = {
+        'only_confirmed': True,
+        'min_timestamp': min_timestamp,
+        'max_timestamp': max_timestamp,
+        'order_by': 'block_timestamp,asc',
+        'limit': 200
+    }
 
-headers = {"Authorization": f"Bearer {api_key}"}
+    headers = {"Authorization": f"Bearer {api_key}"}
 
+    response = requests.get(url, params=params, headers=headers)
+    num = 0
+    reports = []
+    text = f'{yesterday.strftime("%y-%m-%d")} {wallet_name}: [{wallet_address[:3]}_{wallet_address[-3:]}]\nToken: USDT\n'
 
-response = requests.get(url, params=params, headers=headers)
-num = 0
+    if response.status_code == 200:
+        transactions = response.json()["data"]
+        for tr in transactions:
+            num += 1
+            symbol = tr.get('token_info', {}).get('symbol')
+            fr = tr.get('from')
+            to = tr.get('to')
+            v = tr.get('value', '')
+            dec = -1 * int(tr.get('token_info', {}).get('decimals', '6'))
+            f = float(v[:dec] + '.' + v[dec:])
+            time_ = dt.datetime.fromtimestamp(float(tr.get('block_timestamp', '')) / 1000)
+            if fr != wallet_address:
+                reports.append(f"{time_.strftime('%H:%M')}: +{f:>9,.02f} from {fr[:3]}_{fr[-3:]}")
+            else:
+                reports.append(f"{time_.strftime('%H:%M')}: -{f:>9,.02f} to   {to[:3]}_{to[-3:]}")
+    else:
+        print(f"Error: {response.status_code} - {response.text}")
 
-if response.status_code == 200:
-    transactions = response.json()["data"]
-    for transaction in transactions:
-        num += 1
-        print(num, transaction)
-else:
-    print(f"Error: {response.status_code} - {response.text}")
+    for i in reports:
+        text += i + '\n'
+
+    print(text)
+
+if __name__ == '__main__':
+    asyncio.run(get_trongrid_transactions_yesterday('TGzNdQBmFqisqsbSwEbBunoBegVQsYALRh', 'рабочий'))
