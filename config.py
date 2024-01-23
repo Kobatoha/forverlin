51f34321baf93d810b78bfe3ecf15c2fa7400dcb
Alexandra <kobatoha@gmail.com>
2024-01-23 23:52:42 +0300

    upd

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 # telegram bot token
-TOKEN = '6551664026:AAHGN1_4zfC4Hd5HF_IeAL3Fgy2egVyACkM'
+TOKEN = 'token_telegram'
 
 # PostgreSQL db information
 host = 'localhost'
 user = 'postgres'
-password = 'sirok123'
-db_name = 'forverlin'
+password = 'postgres'
+db_name = 'postgres'
 port = 5433
 
 DB_URL = f'postgresql://{user}:{password}@{host}:{port}/{db_name}'
 
-trongrid_api_key = '88b905ca-b432-4f4e-bd6a-09ed49629c4a'
+trongrid_api_key = 'trongrid_api_key'
